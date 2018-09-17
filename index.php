8f0e172b6274dcffcb6d7a97cdd4fd68c0913f12
GanYabS <43345847+GanYabS@users.noreply.github.com>
2018-09-17 21:38:47 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <meta http-equiv="Refresh" content="5; url=https://www.quackit.com/html/tags/">
+  </head>
+  <body>
+    <h1>
+      W8 For Redirecting
+    </h1>
+  </body>
+</html>
